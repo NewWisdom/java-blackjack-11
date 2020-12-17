7d8d089d5b851eeae95b4c24be9dce8ddf22142f
NewWisdom <wlgp2500@gmail.com>
2020-12-18 00:42:04 +0900

    refactor(user-repository): dealer 객체도 저장하도록 변경

--- src/main/java/domain/user/UserRepository.java
+++ src/main/java/domain/user/UserRepository.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class PlayerRepository {
+public class UserRepository {
+    private static final Dealer dealer = new Dealer();
     private static final List<Player> players = new ArrayList<>();
+
+    public static Dealer dealer() {
+        return dealer;
+    }
 
     public static List<Player> players() {
         return Collections.unmodifiableList(players);
     }
 
-    public static void addPlayer(Player player){
+    public static void addPlayer(Player player) {
         players.add(player);
     }
 }
